4d4ff08c91f82a97e20ccb76e6b2f378e8e7b3c4
PriyaMakadia <97655718+PriyaMakadia@users.noreply.github.com>
2022-01-28 14:04:06 +0530

    Add files via upload

---
+++ FindRunner-upFromGivenList.py
@@ -0,0 +1,19 @@
+N= int(input())
+
+list1=map(int,input().split())
+
+list1 = sorted(list1)
+new_list = []
+
+for i in list1:
+    if i not in new_list:
+        new_list.append(i)
+
+if len(new_list)>1:
+    print(new_list[len(new_list)-2])
+
+
+
+# Priya Makadia
+# 20CE049
+
